cd4959181ec78f995c3bbcad8579942af1d13095
EduuD1as <edudiasbianchini@gmail.com>
2024-11-18 20:54:11 -0300

    last commit

--- app/src/main/java/com/example/aula_08_11_app_conversao/MainActivity.java
+++ app/src/main/java/com/example/aula_08_11_app_conversao/MainActivity.java
@@ -46,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
                 case "Real -> Dolar":
                     opcaoSelecionada = "BRL-USD";
                     break;
+                case "Bitcoin -> Real":
+                    opcaoSelecionada = "BTC-BRL";
+                    break;
+                case "Real -> Euro":
+                    opcaoSelecionada = "BRL-EUR";
+                    break;
+                case "Dolar -> Euro":
+                    opcaoSelecionada = "USD-EUR";
+                    break;
                 default:
                     break;
             }
@@ -78,7 +87,18 @@ public class MainActivity extends AppCompatActivity {
                     JsonObject jsonObject = JsonParser.parseString(result).getAsJsonObject();
                     if (jsonObject.has("BRLUSD")) {
                         currencyRate = gson.fromJson(jsonObject.get("BRLUSD"), CurrencyRate.class);
-                    } else {
+                    }
+                    else if (jsonObject.has("BTCBRL")) {
+                        currencyRate = gson.fromJson(jsonObject.get("BTCBRL"), CurrencyRate.class);
+                    }
+                    else if (jsonObject.has("BRLEUR")) {
+                        currencyRate = gson.fromJson(jsonObject.get("BRLEUR"), CurrencyRate.class);
+                    }
+                    else if (jsonObject.has("USDEUR")) {
+                        currencyRate = gson.fromJson(jsonObject.get("USDEUR"), CurrencyRate.class);
+                    }
+
+                    else {
                         Toast.makeText(getApplicationContext(), "Não foi possível encontrar o valor da moeda", Toast.LENGTH_SHORT).show();
                     }
 
